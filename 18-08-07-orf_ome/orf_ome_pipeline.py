e37cd0d47bdb8db535d431c967e2c1cb7537d655
Santiago Medina <santiagogmm@hotmail.com>
2018-08-08 13:10:13 -0500

    added a prefix for putput names and class to run fastqc

--- 18-08-07-orf_ome/orf_ome_pipeline.py
+++ 18-08-07-orf_ome/orf_ome_pipeline.py
@@ -1,7 +1,6 @@
 import luigi
 from os import path
 import subprocess
-import orfomeutlis as utils
 
 
 class Params(luigi.Config):
@@ -11,22 +10,7 @@ class Params(luigi.Config):
     inputFq = luigi.Parameter()  # input fastq file
     adapter5 = luigi.Parameter()  # 5' adapter
     adapter3 = luigi.Parameter()  # 3' adapter
-
-
-class MakeOutputDir(luigi.Task):
-    """
-    makes output dir to save results
-    """
-
-    def requires(self):
-        return list()
-
-    def output(self):
-        return luigi.LocalTarget(utils.sample_name(Params().inputFq))
-
-    def run(self):
-        subprocess.call(f'mkdir {utils.sample_name(Params().inputFq)}',
-                        shell=True)
+    idprefix = luigi.Parameter(default='p-')  # prefix for naming output files
 
 
 class TrimAdapters(luigi.Task):
@@ -35,25 +19,38 @@ class TrimAdapters(luigi.Task):
     """
 
     def requires(self):
-        return MakeOutputDir()
+        return []
 
     def output(self):
-        return luigi.LocalTarget(
-            path.join(utils.sample_name(Params().inputFq),
-                      'trimed.fq')
-        )
+        return luigi.LocalTarget(Params().idprefix + '_trimed.fq')
 
     def run(self):
         """
         run cutadapt to trim the adapters
         """
-        cutadapt_comand = utils.run_cutadapt(
-            Params().inputFq,
-            Params().adapter5,
-            Params().adapter3,
-            utils.sample_name(Params().inputFq)
-        )
-        subprocess.call(cutadapt_comand, shell=True)
+        cutadapt_cmd = f"""
+        cutadapt -j 15 --discard-untrimmed
+        -a {Params().adapter5}...{Params().adapter3}
+        {Params().inputFq} > {Params().idprefix + '_trimed.fq'} 2>
+        {Params().idprefix + '.report1'}
+        """.replace('\n', ' ')
+        subprocess.call(cutadapt_cmd, shell=True)
+
+
+class RunQC(luigi.Task):
+    """"
+    runs quality analysis
+    """
+
+    def requires(self):
+        return TrimAdapters()
+
+    def output(self):
+        return luigi.LocalTarget(Params().idprefix + '_fastqc.html')
+
+    def run(self):
+        qc_cmd = f"fastqc {Params().inputFq}"
+        subprocess.call(qc_cmd, shell=True)
 
 
 class MakeIndex(luigi.Task):
@@ -61,6 +58,7 @@ class MakeIndex(luigi.Task):
     builts a salmon index for the barcodes
     NOTE: I set k = 5, since the barcodes are short.
     """
+
     def requires(self):
         return list()
 
@@ -76,14 +74,12 @@ class QuantifyReads(luigi.Task):
     """
     quantify the trimmed reads with salmon
     """
+
     def requires(self):
-        return [MakeIndex(), TrimAdapters()]
+        return [MakeIndex(), TrimAdapters(), RunQC()]
 
     def output(self):
-        return luigi.LocalTarget(path.join(
-            utils.sample_name(Params().inputFq),
-            'quants'
-        ))
+        return luigi.LocalTarget(Params().idprefix + '-quants')
 
     def run(self):
         """
@@ -91,11 +87,10 @@ class QuantifyReads(luigi.Task):
         """
         cmd = f"""
         salmon quant -i data/barcodes_index
-        -l A -r {path.join(utils.sample_name(Params().inputFq),'trimed.fq')}
-        -o {path.join(utils.sample_name(Params().inputFq), 'quants')}
+        -l A -r {Params().idprefix + '_trimed.fq'}
+        -o {Params().idprefix + '_quants'}
         """.replace('\n', ' ')
         subprocess.call(cmd, shell=True)
-
 
 
 if __name__ == '__main__':
